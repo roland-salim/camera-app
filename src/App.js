9f6e906b0fb29ffce3ffd79f03c8a72277de2d57
roland-salim <98741514+roland-salim@users.noreply.github.com>
2022-01-31 13:19:43 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home/Home'
+import Footer from './components/Footer'
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
